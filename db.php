0a78aad3a9e08c3c893f784c1b24c7fdcb42a604
tristan101-neg <projectneg101@gmail.com>
2026-01-28 16:19:46 +0800

    Add all files and updates

--- db.php
+++ db.php
@@ -3,7 +3,7 @@
 $DB_HOST = "sql200.infinityfree.com";
 $DB_USER = "if0_41001723";
 $DB_PASS = "yifUlIGPu7"; // Set your password if any
-$DB_NAME = "if0_41001723_XXX";
+$DB_NAME = "if0_41001723_cashier_db";
 
 // Create connection
 $conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
